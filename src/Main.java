86d43f18463a2e8ca9b9d731a66b63f19aeec74d
Todd Teis <toddteis@gmail.com>
2022-09-03 08:39:53 +1000

    Added input checking to calculateGrossYearSalary().

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,21 @@ public class Main {
     private static Double calculateGrossYearSalary(Double hoursWorkedPerWeek, Double hourlyRate) {
         Double result;
         Double totalYearlyHours;
+        Scanner keyboard = new Scanner(System.in);
+
+        while(hoursWorkedPerWeek <= 0) {
+            System.out.print("Please enter a positive number for hours worked per week:\t");
+            hoursWorkedPerWeek = keyboard.nextDouble();
+        }
+
+        while(hourlyRate <= 0) {
+            System.out.print("Please enter a positive number for hourly rate:\t");
+            hourlyRate = keyboard.nextDouble();
+        }
+
         totalYearlyHours = hoursWorkedPerWeek * 52;
         result = totalYearlyHours * hourlyRate;
+        keyboard.close();
         return result;
     }    
     public static void main(String[] args) {
